38a48fc52e73f6124d725deecd524a0706293f1c
yu <yu@yu-PC>
2018-06-14 00:50:29 +0800

    注释掉一行代码，添加一行注释

--- nettybook2-master/src/com/phei/netty/basic/TimeClient.java
+++ nettybook2-master/src/com/phei/netty/basic/TimeClient.java
@@ -30,12 +30,13 @@ import io.netty.channel.socket.nio.NioSocketChannel;
  * @version 1.0
  */
 public class TimeClient {
+	//添加内容
 
     public void connect(int port, String host) throws Exception {
 	// 配置客户端NIO线程组
 	EventLoopGroup group = new NioEventLoopGroup();
 	try {
-	    Bootstrap b = new Bootstrap();
+	  //  Bootstrap b = new Bootstrap();
 	    b.group(group).channel(NioSocketChannel.class)
 		    .option(ChannelOption.TCP_NODELAY, true)
 		    .handler(new ChannelInitializer<SocketChannel>() {
